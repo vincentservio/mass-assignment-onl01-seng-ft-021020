24a2bcdb90bf5663eda2ea289a22567801d5dd08
Vincent Servio <vincentservio@gmail.com>
2020-02-26 20:23:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,12 @@
+require "pry"
+
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
  def initialize(butes)
+   binding.pry
    butes.each{|key, value| self.send (("#{key}="), value)}
  end
 end
