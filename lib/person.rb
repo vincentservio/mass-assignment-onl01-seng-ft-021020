9f0c9c0f2472eba8a0a3c8b050565831f6320b22
Vincent Servio <vincentservio@gmail.com>
2020-02-26 20:25:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
  def initialize(butes)
-   binding.pry
+   #binding.pry
   butes.each {|key, value| self.send(("#{key}="), value)}
  end
 end
